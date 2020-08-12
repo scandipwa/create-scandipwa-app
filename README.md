d21159f4a9c575bd0d8f1efe661d0b450b254717
Alfreds Genkins <alfreds@scandipwa.com>
2020-08-12 17:46:33 +0300

    Works but why>

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Install `craco-scandipwa-scripts` dependencies, use: `yarn`
+2. Install `test` dependencies, use `yarn`
+3. 
---
+++ packages/scandipwa-scripts/craco.config.js
@@ -0,0 +1,87 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-param-reassign */
+const { ESLINT_MODES } = require('@craco/craco');
+const webpack = require('webpack');
+const path = require('path');
+const sassResourcesLoader = require('craco-sass-resources-loader');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const FallbackPlugin = require('../webpack-fallback-plugin'); // TODO: replace with dependency
+
+module.exports = () => {
+    const abstractStyle = FallbackPlugin.getFallbackPathname('./src/style/abstract/_abstract.scss');
+    // const entryPoint = FallbackPlugin.getFallbackPathname('./src/index.js');
+
+    return {
+        eslint: {
+            mode: ESLINT_MODES.file
+        },
+        babel: {
+            plugins: [
+                // Allow BEM props
+                'transform-rebem-jsx',
+                // Resolve imports like from 'Component/...'
+                [
+                    'module-resolver', {
+                        root: './',
+                        alias: {
+                            Style: './src/style/',
+                            Component: './src/component/',
+                            Route: './src/route/',
+                            Store: './src/store/',
+                            Util: './src/util/',
+                            Query: './src/query/',
+                            Type: './src/type/'
+                        }
+                    }
+                ]
+            ]
+        },
+        webpack: {
+            plugins: [
+                // in development mode, provide simple translations and React
+                new webpack.ProvidePlugin({
+                    __: path.join(__dirname, '../webpack-i18n-plugin/translation-function'), // TODO: replace with dependency
+                    React: 'react'
+                }),
+
+                // Provide BEM specific variables
+                new webpack.DefinePlugin({
+                    'process.env': {
+                        REBEM_MOD_DELIM: JSON.stringify('_'),
+                        REBEM_ELEM_DELIM: JSON.stringify('-')
+                    }
+                })
+            ],
+            configure: (webpackConfig) => {
+                // Remove module scope plugin, it breaks FallbackPlugin and ProvidePlugin
+                webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
+                    (plugin) => plugin.constructor.name !== ModuleScopePlugin.name
+                );
+
+                // Add FallbackPlugin
+                webpackConfig.resolve.plugins.push(new FallbackPlugin());
+
+                // Allow importing .style files without specifying the extension
+                webpackConfig.resolve.extensions.push('.scss');
+
+                // Allow any files for loaders
+                webpackConfig.module.rules[1].include = undefined;
+                webpackConfig.module.rules[2].oneOf[1].include = undefined;
+
+                // Allow having empty entry point
+                // webpackConfig.entry[whenDev(() => 1, 0)] = entryPoint;
+
+                return webpackConfig;
+            }
+        },
+        plugins: [
+            {
+                // Allow using SCSS mixins in any file
+                plugin: sassResourcesLoader,
+                options: {
+                    resources: abstractStyle
+                }
+            }
+        ]
+    };
+};
