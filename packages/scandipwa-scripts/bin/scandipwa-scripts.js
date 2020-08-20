3ad1ccda29a4134977381e4e07086801484c0b86
Alfreds Genkins <alfreds@scandipwa.com>
2020-08-20 13:56:46 +0300

    Colors, console clear! Notification!

--- packages/scandipwa-scripts/bin/scandipwa-scripts.js
+++ packages/scandipwa-scripts/bin/scandipwa-scripts.js
@@ -4,10 +4,15 @@
 
 const spawn = require('cross-spawn');
 const path = require('path');
+const debounce = require('debounce');
 const chokidar = require('chokidar');
 const chalk = require('chalk');
+const kill = require('tree-kill');
+const clearConsole = require('react-dev-utils/clearConsole');
 
 const args = process.argv.slice(2);
+
+const TIMEOUT_BETWEEN_KILL_TRIGGERS = 500;
 
 if (args.length === 0) {
     // eslint-disable-next-line no-console
@@ -21,7 +26,7 @@ let child = null;
 /**
  * Added path to hard-coded CRACO configuration file
  */
-const spawnUndead = (isNoBrowser = false) => {
+const spawnUndead = (isRestarted = false) => {
     /**
      * Send:
      * - SIGKILL to kill child and parent immediately
@@ -35,40 +40,59 @@ const spawnUndead = (isNoBrowser = false) => {
             '--config', path.join(__dirname, '../craco.config.js')
         ],
         {
-            stdio: 'inherit',
+            stdio: ['inherit', 'pipe', 'inherit'],
             env: {
                 ...process.env,
-                BROWSER: isNoBrowser ? 'none' : ''
+                BROWSER: isRestarted ? 'none' : '',
+                FORCE_COLOR: true
             }
         }
     );
 
-    child.on('error', () => {
-        child.kill('SIGKILL');
-    });
+    child.stdout.on('data', (output) => {
+        const string = output.toString();
 
-    child.on('exit', (_, signal) => {
-        switch (signal) {
-        case 'SIGINT':
-            console.log(chalk.bgYellow('New override detected – restarting...'));
-            spawnUndead(true);
-            break;
-        case 'SIGKILL':
-        default:
-            process.exit();
+        /**
+         * Simply clear the console.
+         */
+        if (/Starting the development server/gm.test(string)) {
+            clearConsole();
+            return;
+        }
+
+        /**
+         * Clear the console, but print the error
+         */
+        if (/Compiled successfully|Failed to compile/gm.test(string)) {
+            clearConsole();
+        }
+
+        console.log(string);
+
+        /**
+         * Show warning to reload the browser
+         */
+        if (isRestarted && string.includes('To create a production')) {
+            console.log(chalk.yellow('Reload the page to see results!'));
         }
     });
 };
 
 process.on('exit', () => {
-    child.kill('SIGKILL');
+    kill(child.pid, 'SIGTERM');
 });
 
 spawnUndead();
 
-const killChild = () => {
-    child.kill('SIGINT'); // could be bad on Windows
-};
+const killChild = debounce(() => {
+    kill(child.pid, 'SIGTERM', (err) => {
+        if (err) {
+            console.log(err);
+        }
+
+        spawnUndead(true);
+    });
+}, TIMEOUT_BETWEEN_KILL_TRIGGERS);
 
 chokidar
     .watch([
