1f71fdacb839032874d1b06c8c6b8f397724109e
Alfreds Genkins <alfreds@scandiweb.com>
2020-09-06 04:04:07 +0300

    Linter rules change - format, disabled no-for

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -29,7 +29,9 @@ module.exports = {
     ],
     "overrides": [
         {
-            "files": ["*.config.js"],
+            "files": [
+                "*.config.js"
+            ],
             "rules": {
                 "@scandipwa/scandipwa-guidelines/create-config-files": "off"
             }
@@ -42,34 +44,58 @@ module.exports = {
         "@scandipwa/scandipwa-guidelines/use-named-export": "error",
         "@scandipwa/scandipwa-guidelines/create-config-files": "error",
         "import/no-extraneous-dependencies": "off",
-        "simple-import-sort/sort": ["error", {
-            "groups": [
-                ["^\\u0000"], // side effect imports
-                ["^@?[a-z]"], // anything that starts with @ and lowercase
-                ["^[^.]"], // anything but a dot
-                ["^\\."] // starting with dot
-            ]
-        }],
+        "simple-import-sort/sort": [
+            "error",
+            {
+                "groups": [
+                    [
+                        "^\\u0000"
+                    ], // side effect imports
+                    [
+                        "^@?[a-z]"
+                    ], // anything that starts with @ and lowercase
+                    [
+                        "^[^.]"
+                    ], // anything but a dot
+                    [
+                        "^\\."
+                    ] // starting with dot
+                ]
+            }
+        ],
         "sort-imports": "off",
         "import/order": "off",
-        "import/no-cycle": ["error", {
-            "maxDepth": 4
-        }],
+        "import/no-cycle": [
+            "error",
+            {
+                "maxDepth": 4
+            }
+        ],
         "fp/no-let": "error",
         "fp/no-arguments": "error",
-        "fp/no-loops": "error",
+        // "fp/no-loops": "error",
         "fp/no-delete": "error",
         "no-var": "error",
         "css-rcurlyexpected": 0,
-        "react/static-property-placement": ["error", "static public field"],
+        "react/static-property-placement": [
+            "error",
+            "static public field"
+        ],
         "react/jsx-props-no-spreading": "off",
         "react/state-in-constructor": "off",
-        "no-restricted-globals": ["error", "isFinite", "isNaN"],
-        "max-len": [2, {
-            "ignoreComments": true,
-            "ignoreUrls": true,
-            "code": 120
-        }],
+        "no-restricted-globals": [
+            "error",
+            "isFinite",
+            "isNaN"
+        ],
+        "max-len": [
+            2,
+            {
+                "ignoreComments": true,
+                "ignoreUrls": true,
+                "code": 120
+            }
+        ],
         "jest/no-disabled-tests": "warn",
         "jest/no-focused-tests": "error",
         "jest/no-identical-title": "error",
@@ -84,72 +110,214 @@ module.exports = {
         "import/no-unresolved": 0,
         "import/named": 0,
         "no-plusplus": 0,
+        "no-unused-vars": [
+            "error",
+            {
+                "vars": "all",
+                "args": "after-used",
+                "ignoreRestSiblings": false,
+                "argsIgnorePattern": "^_"
+            }
+        ],
         "react/react-in-jsx-scope": 0,
-        "react/jsx-curly-spacing": [2, {"when": "always", "allowMultiline": false, "children": true} ],
+        "react/jsx-curly-spacing": [
+            2,
+            {
+                "when": "always",
+                "allowMultiline": false,
+                "children": true
+            }
+        ],
         "import/no-named-as-default": 0,
         "import/no-named-as-default-member": 0,
         // "react/jsx-max-depth": [2, { "max": 2 }],
-        "react/jsx-no-useless-fragment": [2, { "max": 2 }],
-        "curly": [2, "all"],
-        "brace-style": [2, "1tbs", { "allowSingleLine": false }],
-        "react/sort-comp": [2, {
-            "order": [
-                "type-annotations",
-                "static-variables",
-                "static-methods",
-                "instance-variables",
-                "lifecycle",
-                "everything-else",
-                "render"
-            ]
-        }],
+        "react/jsx-no-useless-fragment": [
+            2,
+            {
+                "max": 2
+            }
+        ],
+        "curly": [
+            2,
+            "all"
+        ],
+        "brace-style": [
+            2,
+            "1tbs",
+            {
+                "allowSingleLine": false
+            }
+        ],
+        "react/sort-comp": [
+            2,
+            {
+                "order": [
+                    "type-annotations",
+                    "static-variables",
+                    "static-methods",
+                    "instance-variables",
+                    "lifecycle",
+                    "everything-else",
+                    "render"
+                ]
+            }
+        ],
         "react/jsx-filename-extension": 0,
         "react/prefer-stateless-function": 0,
         "react/button-has-type": 0,
         "react/jsx-indent": 0,
         "react/jsx-indent-props": 2,
-        "react/jsx-no-bind": [2, {
-            "ignoreDOMComponents": false,
-            "ignoreRefs": true,
-            "allowArrowFunctions": false,
-            "allowFunctions": false,
-            "allowBind": false
-        }],
-        "react/forbid-prop-types": [2, {"forbid": ["className"]}],
-        "react/forbid-component-props": [2, {"forbid": ["className"]}],
-        "react/forbid-dom-props": [2, {"forbid": ["className"]}],
+        "react/jsx-no-bind": [
+            2,
+            {
+                "ignoreDOMComponents": false,
+                "ignoreRefs": true,
+                "allowArrowFunctions": false,
+                "allowFunctions": false,
+                "allowBind": false
+            }
+        ],
+        "react/forbid-prop-types": [
+            2,
+            {
+                "forbid": [
+                    "className"
+                ]
+            }
+        ],
+        "react/forbid-component-props": [
+            2,
+            {
+                "forbid": [
+                    "className"
+                ]
+            }
+        ],
+        "react/forbid-dom-props": [
+            2,
+            {
+                "forbid": [
+                    "className"
+                ]
+            }
+        ],
         "react/no-deprecated": 2,
         "babel/semi": 1,
-        "new-cap": ["error", { "newIsCap": true }],
-        "no-param-reassign": ["error", { "props": true, "ignorePropertyModificationsFor": ["acc", "sum"] }],
-        "no-magic-numbers": ["error", { "ignore": [1, 0, 2, -1] }],
+        "new-cap": [
+            "error",
+            {
+                "newIsCap": true
+            }
+        ],
+        "no-param-reassign": [
+            "error",
+            {
+                "props": true,
+                "ignorePropertyModificationsFor": [
+                    "acc",
+                    "sum"
+                ]
+            }
+        ],
+        "no-magic-numbers": [
+            "error",
+            {
+                "ignore": [
+                    1,
+                    0,
+                    2,
+                    -1
+                ]
+            }
+        ],
         "indent": [
-        "error",
-        4,
-        {
-            "ignoredNodes": ["JSXElement","JSXElement > *","JSXAttribute","JSXIdentifier","JSXNamespacedName","JSXMemberExpression","JSXSpreadAttribute","JSXExpressionContainer","JSXOpeningElement","JSXClosingElement","JSXText","JSXEmptyExpression","JSXSpreadChild"]
-        }
+            "error",
+            4,
+            {
+                "ignoredNodes": [
+                    "JSXElement",
+                    "JSXElement > *",
+                    "JSXAttribute",
+                    "JSXIdentifier",
+                    "JSXNamespacedName",
+                    "JSXMemberExpression",
+                    "JSXSpreadAttribute",
+                    "JSXExpressionContainer",
+                    "JSXOpeningElement",
+                    "JSXClosingElement",
+                    "JSXText",
+                    "JSXEmptyExpression",
+                    "JSXSpreadChild"
+                ]
+            }
         ],
         "comma-dangle": [
-        "error",
-        "never"
+            "error",
+            "never"
         ],
         "no-case-declarations": "off",
         "jsx-a11y/label-has-for": 0,
         "padding-line-between-statements": [
             "error",
-            { "blankLine": "always", "prev": ["const", "let", "var"], "next": "*" },
-            { "blankLine": "any", "prev": ["const", "let", "var"], "next": ["const", "let", "var"] },
-            { "blankLine": "always", "prev": ["block", "block-like", "multiline-block-like", "multiline-expression", "multiline-const", "multiline-let", "multiline-var"], "next": "return" },
-            { "blankLine": "any", "prev": ["singleline-const", "singleline-let", "singleline-var"], "next": "*" }
+            {
+                "blankLine": "always",
+                "prev": [
+                    "const",
+                    "let",
+                    "var"
+                ],
+                "next": "*"
+            },
+            {
+                "blankLine": "any",
+                "prev": [
+                    "const",
+                    "let",
+                    "var"
+                ],
+                "next": [
+                    "const",
+                    "let",
+                    "var"
+                ]
+            },
+            {
+                "blankLine": "always",
+                "prev": [
+                    "block",
+                    "block-like",
+                    "multiline-block-like",
+                    "multiline-expression",
+                    "multiline-const",
+                    "multiline-let",
+                    "multiline-var"
+                ],
+                "next": "return"
+            },
+            {
+                "blankLine": "any",
+                "prev": [
+                    "singleline-const",
+                    "singleline-let",
+                    "singleline-var"
+                ],
+                "next": "*"
+            }
         ],
-        "prefer-destructuring": ["error", {
-            "array": false,
-            "object": true
-        }, {
-            "enforceForRenamedProperties": false
-        }],
-        "lines-between-class-members": ["error", "always"],
+        "prefer-destructuring": [
+            "error",
+            {
+                "array": false,
+                "object": true
+            },
+            {
+                "enforceForRenamedProperties": false
+            }
+        ],
+        "lines-between-class-members": [
+            "error",
+            "always"
+        ],
         "no-extra-semi": "error"
     }
 };
