d154130927e2536bef58d5d085063db8314a295d
Alfreds Genkins <alfreds@scandiweb.com>
2020-09-29 09:54:29 +0300

    Patched theme validation issue

--- packages/scandipwa-dev-utils/validate-theme.js
+++ packages/scandipwa-dev-utils/validate-theme.js
@@ -2,21 +2,26 @@ const path = require('path');
 const logger = require('./logger');
 
 const isTheme = (pathname, quiet = false) => {
-    const packageJson = require(`${pathname}/package.json`);
-    const { scandipwa: { type } = {} } = packageJson;
-    const isTheme = type === 'theme';
+    try {
+        const packageJson = require(`${pathname}/package.json`);
+        const { scandipwa: { type } = {} } = packageJson;
+        const isTheme = type === 'theme';
 
-    if (quiet || isTheme) {
-        // Always simply return, if it is quiet, or if the validation passed
-        return isTheme;
+        if (quiet || isTheme) {
+            // Always simply return, if it is quiet, or if the validation passed
+            return isTheme;
+        }
+    } catch (e) {
+        // TODO: do nothing for now
     }
 
     logger.error(
         `Unable to determine if ${ logger.style.file(pathname) } is a ScandiPWA theme.`,
+        '',
         'The folder is considered a theme, if:',
         `- it contains ${ logger.style.misc('package.json') } file`,
-        `- ${ logger.style.misc('package.json') } has a field ${ logger.style.misc('scandipwa.type') }`,
-        `- Field ${ logger.style.misc('scandipwa.type') } value must be equal to ${ logger.style.misc('theme') }`,
+        `- a ${ logger.style.misc('package.json') } field ${ logger.style.misc('scandipwa.type') } must exist`,
+        `- value of field ${ logger.style.misc('scandipwa.type') } must be equal to ${ logger.style.misc('theme') }`,
     );
 
     return false;
