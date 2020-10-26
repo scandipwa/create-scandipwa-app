9e1f16f06f4bcb4118547a7af12fb6bacdc3bb64
ejnshtein <ejnshtein@gmail.com>
2020-10-26 15:02:09 +0200

    move bin to bin folder

--- packages/scandipwa-be-scripts/bin/be-scripts.js
+++ packages/scandipwa-be-scripts/bin/be-scripts.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const commands = {
-    start: () => require('./start')(),
-    stop: () => require('./stop')(),
-    cleanup: () => require('./clean-up')(),
-    restart: () => require('./restart')()
+    start: () => require('../start')(),
+    stop: () => require('../stop')(),
+    cleanup: () => require('../clean-up')(),
+    restart: () => require('../restart')()
 };
 
 const args = process.argv.slice(2);
