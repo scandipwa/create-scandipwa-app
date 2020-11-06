cce772e87b5883d580b3fd95a89a25fe66ea9596
ejnshtein <ejnshtein@gmail.com>
2020-11-06 12:04:13 +0200

    update m2-theme index.php

--- runtime-packages/m2-theme/public/index.php
+++ runtime-packages/m2-theme/public/index.php
@@ -2,20 +2,25 @@
     $colorConfig = $this->getThemeConfiguration('color_customization');
     $contentConfig = $this->getThemeConfiguration('content_customization');
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
-    
-    <script>
-        window.actionName = {
-            type: `<?= $this->getAction(); ?>`
-        };
+<html lang="<?= $this->getLocaleCode() ?>">
+    <head>
+        <meta charset="utf-8">
+        <script>
+            (function() {
+                if (typeof globalThis === 'object') return;
+                Object.prototype.__defineGetter__('__magic__', function() {
+                    return this;
+                });
+                __magic__.globalThis = __magic__;
+                delete Object.prototype.__magic__;
+            }());
 
-        window.contentConfiguration = <?= json_encode($contentConfig) ?> || {};
-    </script>
+            window.actionName = { type: `<?= $this->getAction(); ?>` };
+            window.contentConfiguration = <?= json_encode($contentConfig) ?> || {};
+            window.storeList = JSON.parse(`<?= $this->getStoreListJson() ?>`);
+            window.storeRegexText = `/(${window.storeList.join('|')})?`;
+        </script>
 
     <style>
         <?php if ($colorConfig['enable_color_customization']['enable_custom_colors'] !== "0"): ?>
