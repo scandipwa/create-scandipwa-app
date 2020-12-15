2ad0519d065912c81470b1fbfcde4c5b6dc0a853
Alfreds Genkins <alfreds@scandipwa.com>
2020-12-15 13:48:34 +0200

    Patched extension linking

--- build-packages/scandipwa-scripts/lib/link-extensions.js
+++ build-packages/scandipwa-scripts/lib/link-extensions.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const path = require('path');
 
 const shouldUseYarn = require('@scandipwa/scandipwa-dev-utils/should-use-yarn');
@@ -6,20 +5,32 @@ const execCommandAsync = require('@scandipwa/scandipwa-dev-utils/exec-command');
 const { getPackageJson } = require('@scandipwa/scandipwa-dev-utils/package-json');
 
 module.exports = async () => {
-    const packagesPath = path.join(process.cwd(), 'packages');
+    const { dependencies } = getPackageJson(process.cwd());
 
-    if (!fs.existsSync(packagesPath)) {
-        // no packages - no deal
+    if (!dependencies) {
         return;
     }
 
-    const linkPromises = fs.readdirSync(packagesPath).map(async (folder) => {
-        const packagePath = path.join(packagesPath, folder);
-        const { name } = getPackageJson(packagePath);
-        const command = shouldUseYarn() ? 'yarnpkg' : 'npm';
-        await execCommandAsync(command, ['link'], packagePath);
-        await execCommandAsync(command, ['link', name], process.cwd());
-    });
+    const command = shouldUseYarn() ? 'yarnpkg' : 'npm';
+
+    const linkPromises = Object.entries(dependencies).reduce((acc, [name, version]) => {
+        if (!version.startsWith('file:')) {
+            // skip all non file dependencies
+            return acc;
+        }
+
+        // to get path from file:/path trim first 5 symbols
+        const packagePath = path.join(process.cwd(), version.slice(5));
+
+        // call async IIFE to get the promise
+        const linkPromise = (async () => {
+            await execCommandAsync(command, ['link'], packagePath);
+            await execCommandAsync(command, ['link', name], process.cwd());
+        })();
+
+        // populate the array with new link promise
+        return [...acc, linkPromise];
+    }, []);
 
     await Promise.all(linkPromises);
 };
