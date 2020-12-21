d4d13f5d3142c45111e771ea30c803e3aa01500d
yeegor <yegor.batov@gmail.com>
2020-12-21 11:09:46 +0200

    Ref: transform function declarations to assignment expressions

--- build-packages/webpack-i18n-runtime/build-config/config.plugin.js
+++ build-packages/webpack-i18n-runtime/build-config/config.plugin.js
@@ -3,7 +3,7 @@ const path = require('path');
 const WebpackI18nTracker = require('./webpack-i18n-tracker');
 
 // Provide an additional loader directory from this module
-function addResolveLoader(config) {
+const addResolveLoader = (config) => {
     if (!config.resolveLoader) {
         config.resolveLoader = {};
     }
@@ -14,18 +14,18 @@ function addResolveLoader(config) {
         config.resolveLoader.modules.push('node_modules');
     }
     config.resolveLoader.modules.push(__dirname);
-}
+};
 
 // Load the locale map with import injector
-function addImportInjector(config) {
+const addImportInjector = (config) => {
     config.module.rules.push({
         test: require.resolve(path.join(__dirname, '../src/util/localeMap.js')),
         loader: 'webpack-i18n-import-loader'
     });
-}
+};
 
 // Provide the __ function with the ProvidePlugin
-function provideTranslationFunction(config) {
+const provideTranslationFunction = (config) => {
     config.plugins.forEach(((plugin) => {
         // if (plugin instanceof webpack.ProvidePlugin) {
         const { __proto__: { constructor: { name } = {} } = {} } = plugin;
@@ -33,14 +33,14 @@ function provideTranslationFunction(config) {
             plugin.definitions.__ = [require.resolve(path.join(__dirname, '../src/util/__.js')), 'default'];
         }
     }));
-}
+};
 
 // Add the plugin for missing/unused translation handling
-function addTrackerPlugin(config) {
+const addTrackerPlugin = (config) => {
     config.plugins.push(new WebpackI18nTracker({
         defaultLocale: 'en_US'
     }));
-}
+};
 
 module.exports = {
     plugin: {
