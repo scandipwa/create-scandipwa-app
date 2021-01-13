3e3eb56e8f06cfa2ae28caabc6cba0e20d9fe221
yeegor <yegor.batov@gmail.com>
2021-01-13 12:26:12 +0200

    Improve plugin returns' validation

---
+++ build-packages/scandipwa-template-plugin-system/util/ensure-valid-value.js
@@ -0,0 +1,32 @@
+const { STRING_API_KEY, DOM_API_KEY } = require('./exposed-api-keys');
+
+const PluginInvalidDomException = require('../errors/plugin-invalid-dom');
+const PluginInvalidStringException = require('../errors/plugin-invalid-string');
+
+const exceptionMap = {
+    [DOM_API_KEY]: PluginInvalidDomException,
+    [STRING_API_KEY]: PluginInvalidStringException
+};
+
+const faultyCaseMap = (value) => ({
+    [DOM_API_KEY]: [
+        !value,
+        typeof value !== 'object',
+        JSON.stringify(value) === '{}',
+        Array.isArray(value)
+    ],
+    [STRING_API_KEY]: [
+        !value,
+        typeof value !== 'string'
+    ]
+});
+
+const ensureValidValue = (apiType, value) => {
+    if (faultyCaseMap(value)[apiType].find(Boolean)) {
+        throw new exceptionMap[apiType](value);
+    }
+
+    return value;
+};
+
+module.exports = ensureValidValue;
