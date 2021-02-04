79b51df91fee61f0bd1a0f0cc6b3137b8c8b768f
yeegor <yegor.batov@gmail.com>
2021-02-04 13:04:42 +0200

    Rename the creator

--- build-packages/scandipwa-cli/actions/create/index.js
+++ build-packages/scandipwa-cli/actions/create/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
 const { ResourceType } = require('@scandipwa/scandipwa-development-toolkit-core');
 
+const creator = require('./lib/creator');
 const componentOptions = require('./lib/options/componentOptions');
 const storeOptions = require('./lib/options/storeOptions');
-const componentCreator = require('./lib/creator');
 
 module.exports = (yargs) => {
     yargs.command('create <resource type>', 'Create a new resource', (yargs) => {
-        yargs.command('component <name>', 'Create a component', componentOptions, componentCreator(ResourceType.Component));
-        yargs.command('route <name>', 'Create a route', componentOptions, componentCreator(ResourceType.Route));
-        yargs.command('store <name>', 'Create a store', storeOptions, componentCreator(ResourceType.Store));
+        yargs.command('component <name>', 'Create a component', componentOptions, creator(ResourceType.Component));
+        yargs.command('route <name>', 'Create a route', componentOptions, creator(ResourceType.Route));
+        yargs.command('store <name>', 'Create a store', storeOptions, creator(ResourceType.Store));
     });
 };
