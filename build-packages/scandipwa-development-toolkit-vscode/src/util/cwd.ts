1f1fc98fd3562910bbc228a39b568ff13e16a9d9
yeegor <yegor.batov@gmail.com>
2021-02-10 17:49:51 +0200

    Provide on-button text on Mac

--- build-packages/scandipwa-development-toolkit-vscode/src/util/cwd.ts
+++ build-packages/scandipwa-development-toolkit-vscode/src/util/cwd.ts
@@ -3,7 +3,10 @@ import * as vscode from 'vscode';
 import { proposeFromHistory, unshiftUniqueToHistory } from './history';
 import locateScandipwaModule from '@scandipwa/scandipwa-dev-utils/locate-scandipwa-module';
 
-export const selectDirectoryWithHistory = async (message: string, historyKey: string) => {
+export const selectDirectoryWithHistory = async (
+    message: string, 
+    historyKey: string
+) => {
     const selectedFromHistory = await proposeFromHistory(historyKey, message);
 
     // Handle selected one of previously selected ones
@@ -11,17 +14,25 @@ export const selectDirectoryWithHistory = async (message: string, historyKey: st
         return locateScandipwaModule(selectedFromHistory);
     }
 
-    // Provide selection with FS
-    const selectedDirectories = await vscode.window.showOpenDialog({
+    // Initialize for non-mac
+    const options: vscode.OpenDialogOptions = {
         canSelectFolders: true,
         canSelectFiles: false,
         canSelectMany: false,
         title: message
-    });
+    };
+
+    // On Mac, open window does not have a title
+    if (process.platform === 'darwin') {
+        options.openLabel = message;
+    }
+
+    // Provide selection with FS
+    const selectedDirectories = await vscode.window.showOpenDialog(options);
 
     // Handle not selecting anything
     if (!selectedDirectories) {
-        vscode.window.showErrorMessage('Please, select a directory to create functionality in!');
+        vscode.window.showErrorMessage('Select a directory to create functionality in');
 
         return null;
     }
