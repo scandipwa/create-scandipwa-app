51192465d458d37c08465eb1ee0acefc162e9ccd
yeegor <yegor.batov@gmail.com>
2021-02-16 18:03:04 +0200

    Provide opportunity to control import sequence in entry

--- build-packages/scandipwa-extensibility/build-config/webpack-extension-import-helper-loader/index.js
+++ build-packages/scandipwa-extensibility/build-config/webpack-extension-import-helper-loader/index.js
@@ -1,26 +1,43 @@
 const path = require('path');
 
+const getSlicePoint = (source) => {
+    // Attempt to match by hook
+    const hookMatcher = '/** ENTRY__HOOK */';
+    const hookPosition = source.indexOf(hookMatcher);
+
+    if (hookPosition) {
+        return hookPosition;
+    }
+
+    // Attempt to match by imports
+    const importMatcher = /^import .+$/gm;
+    const imports = source.match(importMatcher);
+
+    // If no imports => make one
+    if (!imports || !imports.length) {
+        return 0;
+    }
+
+    // There are imports => take the first one
+    const firstImport = imports[0];
+    const firstImportPosition = source.indexOf(firstImport);
+
+    return firstImportPosition;
+};
+
 /**
  * This will import the neighboring runtime-helpers into the application
  * The import will be the last import in the application's entry point
  * But it will occur before ReactDOM.render()
  */
 module.exports = function injectImports(source) {
-    const injectablePath = path
-        .resolve(__dirname, '..', '..', 'runtime-helpers')
-        .split(path.sep)
-        .join(path.posix.sep);
-
+    const injectablePath = path.resolve(__dirname, '..', '..', 'runtime-helpers');
     const injectableCode = `import '${injectablePath}';\n`;
 
-    const importMatcher = /^import .+$/gm;
-    const imports = source.match(importMatcher);
+    const slicePoint = getSlicePoint(source);
 
-    const firstImport = imports[0];
-    const firstImportPosition = source.indexOf(firstImport);
-
-    const codeBeforeInjectable = source.slice(0, firstImportPosition);
-    const codeAfterInjectable = source.slice(firstImportPosition);
+    const codeBeforeInjectable = source.slice(0, slicePoint);
+    const codeAfterInjectable = source.slice(slicePoint);
 
     const injectedCode = [
         codeBeforeInjectable,
