5ee8d5978cbd0a81ec85e3e4e70024c5b8c7a9ae
yeegor <yegor.batov@gmail.com>
2021-02-24 13:06:26 +0200

    Comment build config plugin

--- build-packages/scandipwa-extensibility/build-config/config.plugin.js
+++ build-packages/scandipwa-extensibility/build-config/config.plugin.js
@@ -1,3 +1,8 @@
+/**
+ * This file provides the e11y functionality for the CSA setup
+ * It has no effect for applications without the build configuration plugin system
+ */
+
 const injectBabelConfig = require('./util/babel');
 const injectWebpackConfig = require('./util/webpack');
 
