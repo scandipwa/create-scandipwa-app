1789a3170982ede227e5e5903b16ab011d0920dd
Alfreds Genkins <alfreds@scandiweb.com>
2021-03-16 17:10:28 +0400

    Added post-install announcement

--- build-packages/csa-generator-theme/index.js
+++ build-packages/csa-generator-theme/index.js
@@ -17,6 +17,29 @@ const ensureLatestComposer = (pathname) => {
     writeJson(composerPath, composerJson);
 };
 
+const makeAnAnnouncement = () => {
+    const eventDate = Date.parse('2021-03-26');
+
+    if (new Date() > eventDate) {
+        return;
+    }
+
+    logger.log(); // add empty line
+
+    logger.log(logger.style.comment('==================================='));
+    logger.log(); // add empty line
+    logger.log('Hey friend! A small announcement:');
+    logger.log(); // add empty line
+    logger.log(`There is a ScandiPWA Spring meetup organized on the ${ logger.style.code('26th of March!') }`);
+    logger.logT('- Greet the team behind ScandiPWA!');
+    logger.logT('- Meet the community propulsing ScandiPWA forward!');
+    logger.logT('- Learn how ScandiPWA impacts business – directly from business owners!');
+    logger.log(); // add empty line
+    logger.log('Register for free:', logger.style.link('https://hopin.com/events/scandipwa-spring-meetup-2021'));
+    logger.log(); // add empty line
+    logger.log(logger.style.comment('==================================='));
+};
+
 const greet = (
     name,
     pathname
@@ -46,6 +69,8 @@ const greet = (
     logger.log('We suggest that you begin by typing:');
     logger.logT(logger.style.command('cd'), relativePathname);
     logger.logT(logger.style.command(`${displayedCommand} start`));
+
+    makeAnAnnouncement();
 
     logger.log(); // add empty line
     logger.logN('Happy coding! <3');
