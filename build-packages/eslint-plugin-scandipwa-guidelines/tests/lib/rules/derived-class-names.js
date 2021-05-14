e4097f18edce73feb4750c089d0da04940df5d19
Reinis Mazeiks <reinis.mazeiks@scandiweb.com>
2021-05-14 14:40:07 +0300

    Add rule tests for derived-class-names.js

---
+++ build-packages/eslint-plugin-scandipwa-guidelines/tests/lib/rules/derived-class-names.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable scandipwa-extensibility/no-non-extensible-components */
+
+const { RuleTester } = require("eslint");
+const rule = require("../../../lib/rules/derived-class-names");
+
+const ruleTester = new RuleTester({
+    parserOptions: { ecmaVersion: 2015, sourceType: "module" },
+    env: {
+        es6: true,
+    },
+});
+ruleTester.run("derived-class-names", rule, {
+    valid: [
+        {
+            code: "class Anything {}",
+            filename: "Hello.js",
+        },
+        {
+            code: "class FooterComponent {}",
+            filename: "Footer.component.js",
+        },
+        {
+            code: "class MyTest {}",
+            filename: "my.test.js",
+        },
+    ],
+
+    invalid: [
+        {
+            code: "class HelloComponent {}",
+            filename: "Goodbye.component.js",
+            output: "class GoodbyeComponent {}",
+            errors: [{ rule: "derived-class-names" }],
+        },
+        {
+            code: "export class FooterComponent {}",
+            filename: "Footer.container.js",
+            output: "export class FooterContainer {}",
+            errors: [{ rule: "derived-class-names" }],
+        },
+        {
+            code: "class Test {} export default Test;",
+            filename: "my.test.js",
+            output: "class MyTest {} export default Mytest;",
+            errors: [{ rule: "derived-class-names" }],
+        },
+    ],
+});
