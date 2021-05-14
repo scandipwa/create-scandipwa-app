9f27692b209b292155a5b014dfe896838bce5ba7
Reinis Mazeiks <reinis.mazeiks@scandiweb.com>
2021-05-14 16:34:19 +0300

    Add more tests for derived-class names
    Corner cases when the same identifier appears in several places.

--- build-packages/eslint-plugin-scandipwa-guidelines/tests/lib/rules/derived-class-names.js
+++ build-packages/eslint-plugin-scandipwa-guidelines/tests/lib/rules/derived-class-names.js
@@ -45,5 +45,17 @@ ruleTester.run("derived-class-names", rule, {
             output: "class MyTest {} export default MyTest;",
             errors: [{ rule: "derived-class-names" }, { rule: "derived-class-names" }],
         },
+        {
+            code: "class Food {} export default connect(null, null)(Food);",
+            filename: "Food.container.js",
+            output: "class FoodContainer {} export default connect(null, null)(FoodContainer);",
+            errors: [{ rule: "derived-class-names" }, { rule: "derived-class-names" }],
+        },
+        {
+            code: "import { Logo as BaseLogo } from 'SourceComponent/Logo/Logo.component'; class Logo {}",
+            filename: "Logo.component.js",
+            output: "import { Logo as BaseLogo } from 'SourceComponent/Logo/Logo.component'; class LogoComponent {}",
+            errors: [{ rule: "derived-class-names" }],
+        },
     ],
 });
