46c9bc784eb217f2b2c47d1aac42189b087a4ab6
Reinis Mazeiks <reinis.mazeiks@scandiweb.com>
2021-05-26 10:00:04 +0300

    Additional tests for use-namespace

--- build-packages/eslint-plugin-scandipwa-guidelines/tests/lib/rules/use-namespace.js
+++ build-packages/eslint-plugin-scandipwa-guidelines/tests/lib/rules/use-namespace.js
@@ -22,5 +22,33 @@ ruleTester.run("use-namespace", rule, {
                 "export class HeaderComponent {}",
             errors: 1,
         },
+        {
+            code: "/** @namespace The/Wrong/Namespace/HeaderComponent */ export class HeaderComponent {}",
+            output: "/** @namespace TestPackage/Test/Path/HeaderComponent */ export class HeaderComponent {}",
+            errors: 1,
+        },
+        {
+            code: "/** @namespace TestPackage/Test/Path/WrongExportName */ export class HeaderComponent {}",
+            output: "/** @namespace TestPackage/Test/Path/HeaderComponent */ export class HeaderComponent {}",
+            errors: 1,
+        },
+        {
+            code: "/**\n" +
+                "Some documentation comment\n" +
+                "*/\n" +
+                "export class HeaderComponent {}",
+            output: "/**\n" +
+                "Some documentation comment\n" +
+                "* @namespace TestPackage/Test/Path/HeaderComponent\n" +
+                " */\n" +
+                "export class HeaderComponent {}",
+            errors: 1,
+        },
+        {
+            code: "export const mapStateToProps = () => {};",
+            output: "/** @namespace TestPackage/Test/Path/mapStateToProps */\n" +
+                "export const mapStateToProps = () => {};",
+            errors: 1,
+        },
     ],
 });
