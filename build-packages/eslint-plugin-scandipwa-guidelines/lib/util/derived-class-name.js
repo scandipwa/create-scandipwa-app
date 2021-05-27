e714817fabceb1faf4ceb4d841c05f8dbd9c52c2
Reinis Mazeiks <reinis.mazeiks@scandiweb.com>
2021-05-27 10:02:20 +0300

    Add derived-class-names doc URL Refactor: move message to rule script

--- build-packages/eslint-plugin-scandipwa-guidelines/lib/util/derived-class-name.js
+++ build-packages/eslint-plugin-scandipwa-guidelines/lib/util/derived-class-name.js
@@ -23,17 +23,7 @@ function getExpectedClassNameFromFilename(filename) {
     return withCapitalizedInitial(baseName);
 }
 
-function getUnexpectedNameMessage(filename, expectedName, actualName) {
-    const error = `In Scandi, class names need to be based on the file name. Since the filename is ${ filename } the class name should be ${ expectedName }.`;
-    const help = `To fix this error, rename ${ actualName } to ${ expectedName }.`;
-    const documentationLink =
-        "https://github.com/scandipwa/eslint/blob/master/docs/rules/derived-class-names.md";
-
-    return constructMessage(error, help, documentationLink);
-}
-
 module.exports = {
     shouldClassNameBeEnforced,
     getExpectedClassNameFromFilename,
-    getUnexpectedNameMessage,
 };
